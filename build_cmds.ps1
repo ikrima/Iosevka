235f87a7ba9488d5d783bace014eb62809b186d5
ikrima <github@ikrima.com>
2023-01-19 04:46:42 -0600

    revamp/fix build plans
    Squashed commit of the following:
    
    commit 25109f6a456df7a81987461b905137a3c8515d0d
    Merge: 31acb7c1 409534a8
    Author: ikrima <github@ikrima.com>
    Date:   Thu Jan 19 04:51:06 2023 -0600
    
        Merge branch 'ikrimae-mod' into sanity-check

--- build_cmds.ps1
+++ build_cmds.ps1
@@ -1,27 +1,62 @@
-# Build Commands
-
-
-# Clean Upgrade/Reinstall
+[Diagnostics.CodeAnalysis.SuppressMessage('PSAvoidUsingPositionalParameters','')]
+[Diagnostics.CodeAnalysis.SuppressMessage('PSUseConsistentIndentation','')]
+param()
+#========================================================================================
+# Upgrade/Reinstall
+#----------------------------------------------------------------------------------------
 nukedir .\node_modules
 npm install
-
+#========================================================================================
 # Clean Build
+#----------------------------------------------------------------------------------------
 npm run clean
-
+#========================================================================================
 # Super TTC Composite (recommended)
-npm run build -- super-ttc::iosevka-ikrima            --jCmd=28
-npm run build -- super-ttc::iosevka-ikrima-slab       --jCmd=28
-npm run build -- super-ttc::iosevka-pragmatapro       --jCmd=28
-npm run build -- super-ttc::iosevka-pragmatapro-slab  --jCmd=28
+#----------------------------------------------------------------------------------------
+npm run build --verbose -- --verbosity=8 --jCmd=28 super-ttc::iosevka
+npm run build --verbose -- --verbosity=8 --jCmd=28 super-ttc::iosevka-slab
 
-# TTC for each scope
-npm run build -- ttc::iosevka-ikrima                  --jCmd=28
-npm run build -- ttc::iosevka-ikrima-slab             --jCmd=28
-npm run build -- ttc::iosevka-pragmatapro             --jCmd=28
-npm run build -- ttc::iosevka-pragmatapro-slab        --jCmd=28
+npm run build --verbose -- --verbosity=8 --jCmd=28 super-ttc::iosevka-edev
+npm run build --verbose -- --verbosity=8 --jCmd=28 super-ttc::iosevka-edev-slab
 
-# TTF for each scope (not recommended)
-npm run build -- ttf::iosevka-ikrima                  --jCmd=28
-npm run build -- ttf::iosevka-ikrima-slab             --jCmd=28
-npm run build -- ttf::iosevka-pragmatapro             --jCmd=28
-npm run build -- ttf::iosevka-pragmatapro-slab        --jCmd=28
+npm run build --verbose -- --verbosity=8 --jCmd=28 super-ttc::iosevka-pragpro
+npm run build --verbose -- --verbosity=8 --jCmd=28 super-ttc::iosevka-pragpro-slab
+#========================================================================================
+# TTC for each composite
+#----------------------------------------------------------------------------------------
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttc::iosevka
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttc::iosevka-slab
+
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttc::iosevka-edev
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttc::iosevka-edev-slab
+
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttc::iosevka-pragpro
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttc::iosevka-pragpro-slab
+#========================================================================================
+# TTF for each group (not recommended)
+#----------------------------------------------------------------------------------------
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-term
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-fixed
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-slab
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-term-slab
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-fixed-slab
+
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-edev
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-edev-term
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-edev-fixed
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-edev-slab
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-edev-term-slab
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-edev-fixed-slab
+
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-pragpro
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-pragpro-term
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-pragpro-fixed
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-pragpro-slab
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-pragpro-term-slab
+npm run build --verbose -- --verbosity=8 --jCmd=28 ttf::iosevka-pragpro-fixed-slab
+#========================================================================================
+# TTF+WEB for each group (not recommended)
+#----------------------------------------------------------------------------------------
+npm run build --verbose -- --verbosity=8 --jCmd=28 contents::iosevka-edev
+npx verda -f verdafile.mjs --verbosity=8 --jCmd=28 ttc::iosevka-edev
